281930b105c3f531e12bca8a495984f5e7b22514
Alissa Maisner <arnes.maisner@edeka.de>
2025-08-22 08:57:15 +0200

    moved save with exception handler into separate function to be used in other classes

--- src/SnakeDriver.java
+++ src/SnakeDriver.java
@@ -25,15 +25,19 @@ public class SnakeDriver {
 	// game settings
 	public static Settings settings = Settings.load();
 
-	public static void main(String[] args) {
-		
-		ArgHandler.handleArgs(args);
-
+	public static void saveWithExceptionHandler() {
 		try {
 			settings.save();
 		} catch (IOException e) {
 			System.out.println("unable to save game settings due to reason: " + e.getMessage());
 		}
+	}
+
+	public static void main(String[] args) {
+		
+		ArgHandler.handleArgs(args);
+
+		saveWithExceptionHandler();
 
 		GameLogic logic = new GameLogic();
 		VideoOutput video = new VideoOutput();
